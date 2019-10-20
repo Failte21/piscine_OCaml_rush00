97c5fcdf2f0d14ad7cf3cdcb35baa9033e399aa5
Leo SIMON <lsimon@e2r7p18.42.fr>
2019-10-20 13:51:49 +0200

    result: improved

--- result/Result.ml
+++ result/Result.ml
@@ -1,3 +1,18 @@
 type ('a, 'b) result =
 	| Ok of 'a
-	| Err of 'b
+  | Err of 'b
+
+let flatMap fn result =
+  match result with
+    | Ok el -> fn el
+    | e -> e
+
+let map fn result =
+  match result with
+    | Ok el -> Ok (fn el)
+    | e -> e
+
+let fold on_success on_error result =
+  match result with
+    | Ok el -> on_success el
+    | Err err -> on_error err
